e8dbbf538f59c53d2132ec8ae39f3f9d3f3de79d
Alex <bravoalex004@gmail.com>
2026-03-25 19:54:35 +0100

    Actualitzades les deades mock

--- src/app/mocks/dades-mock.ts
+++ src/app/mocks/dades-mock.ts
@@ -7,10 +7,8 @@ export const ELEMENTS_MOCK: Element[] = [
 	{ id: 4, nom: 'Llegir un llibre', valor: 40 },
 	{ id: 5, nom: 'Preparar el sopar', valor: 50, descripcio: 'Cuinar alguna cosa senzilla' },
 	{ id: 6, nom: 'Trucar als familiars', valor: 60 },
-	{ id: 7, nom: 'Ordenar l'habitació', valor: 70, descripcio: 'Recollir roba i escriptori' },
+	{ id: 7, nom: 'Ordenar l’habitació', valor: 70, descripcio: 'Recollir roba i escriptori' },
 	{ id: 8, nom: 'Fer la compra', valor: 80 },
 	{ id: 9, nom: 'Practicar codi', valor: 90 },
 	{ id: 10, nom: 'Descansar', valor: 100, descripcio: 'Prendre una pausa i desconnectar' }
 ];
-
-
