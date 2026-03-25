f0ec7a6415aa33ae9595556fac46a920f07c8d95
Alex <bravoalex004@gmail.com>
2026-03-25 19:50:51 +0100

    Actualitzades les deades mock

--- src/app/mocks/dades-mock.ts
+++ src/app/mocks/dades-mock.ts
@@ -1,16 +1,16 @@
 import { Element } from '../models/element.model';
 
 export const ELEMENTS_MOCK: Element[] = [
-	{ id: 1, nom: 'Tasca 1', valor: 10, descripcio: 'Primera tasca' },
-	{ id: 2, nom: 'Tasca 2', valor: 20 },
-	{ id: 3, nom: 'Tasca 3', valor: 30 },
-	{ id: 4, nom: 'Tasca 4', valor: 40 },
-	{ id: 5, nom: 'Tasca 5', valor: 50, descripcio: 'Cinquena tasca' },
-	{ id: 6, nom: 'Tasca 6', valor: 60 },
-	{ id: 7, nom: 'Tasca 7', valor: 70 },
-	{ id: 8, nom: 'Tasca 8', valor: 80 },
-	{ id: 9, nom: 'Tasca 9', valor: 90 },
-	{ id: 10, nom: 'Tasca 10', valor: 100, descripcio: 'Desena tasca' }
+	{ id: 1, nom: 'Comprar menjar', valor: 10, descripcio: 'Fruita, pa i llet' },
+	{ id: 2, nom: 'Estudiar', valor: 20 },
+	{ id: 3, nom: 'Fer exercici', valor: 30, descripcio: 'Caminar 30 minuts' },
+	{ id: 4, nom: 'Llegir un llibre', valor: 40 },
+	{ id: 5, nom: 'Preparar el sopar', valor: 50, descripcio: 'Cuinar alguna cosa senzilla' },
+	{ id: 6, nom: 'Trucar als familiars', valor: 60 },
+	{ id: 7, nom: 'Ordenar l'habitació', valor: 70, descripcio: 'Recollir roba i escriptori' },
+	{ id: 8, nom: 'Fer la compra', valor: 80 },
+	{ id: 9, nom: 'Practicar codi', valor: 90 },
+	{ id: 10, nom: 'Descansar', valor: 100, descripcio: 'Prendre una pausa i desconnectar' }
 ];
 
 
