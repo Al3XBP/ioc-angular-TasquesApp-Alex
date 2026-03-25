a6654272bf8db39636469c3729752502cc56f3e5
Alex <bravoalex004@gmail.com>
2026-03-25 19:35:53 +0100

    Afegiits components i comunicació Input/Output amb cerca funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ELEMENTS_MOCK } from './mocks/dades-mock';
 import { Element } from './models/element.model';
+import { ELEMENTS_MOCK } from './mocks/dades-mock';
+import { BarraCercaComponent } from './components/barra-cerca/barra-cerca.component';
+import { LlistaElementsComponent } from './components/llista-elements/llista-elements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BarraCercaComponent, LlistaElementsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   elements: Element[] = ELEMENTS_MOCK;
+  elementsFiltrats: Element[] = ELEMENTS_MOCK;
 
   constructor() {
     console.log('TasquesApp inicialitzada correctament.');
+  }
+
+  actualitzarCerca(text: string): void {
+    const textMin = text.toLowerCase();
+
+    this.elementsFiltrats = this.elements.filter(element =>
+      element.nom.toLowerCase().includes(textMin)
+    );
   }
 }
---
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-barra-cerca',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './barra-cerca.component.html',
+  styleUrl: './barra-cerca.component.scss'
+})
+export class BarraCercaComponent {
+  textCerca: string = '';
+
+  @Output() cercaCanviada = new EventEmitter<string>();
+
+  actualitzarCerca(): void {
+    this.cercaCanviada.emit(this.textCerca);
+  }
+}
