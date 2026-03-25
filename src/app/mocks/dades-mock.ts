fd3a728acb4864fb829e4d1620a93d8aa786f40a
Alex <bravoalex004@gmail.com>
2026-03-25 17:50:36 +0100

    Afegides dades mock

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,16 @@
+import { Element } from '../models/element.model';
+
+export const ELEMENTS_MOCK: Element[] = [
+	{ id: 1, nom: 'Tasca 1', valor 10, descripcio: 'Primera tasca' },
+	{ id: 2, nom: 'Tasca 2', valor 20 },
+	{ id: 3, nom: 'Tasca 3', valor 30 },
+	{ id: 4, nom: 'Tasca 4', valor 40 },
+	{ id: 5, nom: 'Tasca 5', valor 50, descripcio: 'Cinquena tasca' },
+	{ id: 6, nom: 'Tasca 6', valor 60 },
+	{ id: 7, nom: 'Tasca 7', valor 70 },
+	{ id: 8, nom: 'Tasca 8', valor 80 },
+	{ id: 9, nom: 'Tasca 9', valor 90 },
+	{ id: 10, nom: 'Tasca 10', valor 100, descripcio: 'Desena tasca' }
+];
+
+
